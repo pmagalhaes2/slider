4e9cb866c1f4af3a1090bc09a7edeca8c982d2d1
Patricia Magalhães <paath.magalhaes2@gmail.com>
2021-10-29 22:23:36 -0300

    automatic scroll slider

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const previousEl = document.getElementById('previous');
 const nextEl = document.getElementById('next');
 const sliderEl = document.getElementById('slider');
+let interval = undefined;
+let timeout = undefined;
 
 previousEl.addEventListener('click', onPreviousClick);
 nextEl.addEventListener('click', onNextClick);
 
+autoScroll();
+
 function onPreviousClick() {
     const sliderWidth = sliderEl.offsetWidth;
     sliderEl.scrollLeft -= sliderWidth;
+    handleSliderClick()
 }
 
 function onNextClick() {
     const sliderWidth = sliderEl.offsetWidth;
     sliderEl.scrollLeft += sliderWidth;
+    handleSliderClick()
+}
+
+function handleSliderClick() {
+    clearTimeout(timeout);
+    clearInterval(interval);
+    interval = undefined;
+    setTimeout(() => {
+        autoScroll();
+    }, 10000);
+}
+
+
+function autoScroll() {
+    if (interval) return;
+    interval = setInterval(() => {
+            const sliderWidth = sliderEl.offsetWidth;
+            const numberOfImages = sliderEl.childElementCount;
+            const selectedImage = (sliderEl.scrollLeft / sliderWidth) + 1;
+
+            if (numberOfImages === selectedImage) {
+                sliderEl.scrollLeft = 0;
+                return
+            }
+            sliderEl.scrollLeft += sliderWidth;
+
+        },
+        3000);
 }
